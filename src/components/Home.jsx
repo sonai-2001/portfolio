1edf826105a24e72b879d7ea0aa76a723d23bbbe
sonai-2001 <sonaibhadra11174@gmail.com>
2024-07-29 09:49:56 +0530

    nav responsive complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div id='home' className='w-full my-5'>
+         <div id='info' className='max-w-screen-lg px-3 min-h-[50vh] py-1 bg-zinc-700 mx-auto'>
+              
+              <div>
+
+              </div>
+              <div>
+                
+              </div>
+         </div>
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+ 
+ 
+ const[open,setOpen]=useState(false)
+ const handleClick=()=>{
+   setOpen(!open)
+ }
+ const handleLink=()=>{
+      if(open){
+        setOpen(false)
+      }
+ }
   return (
     <div
       id="navbar"
-      className="max-w-screen-xl px-3 py-1 bg-zinc-700 mx-auto flex justify-between items-center rounded-md"
+      className="max-w-screen-xl px-3 py-1 bg-zinc-700 mx-auto flex justify-between items-center rounded-md relative"
     >
       <div
         id="nleft"
-        className="w-[75px] h-[75px] rounded-full border  overflow-hidden"
+        className="w-[70px] md:w-[75px] h-[70px] md:h-[75px] rounded-full border  overflow-hidden"
       >
-      <img id="navimage" className="w-full h-full object-cover object-left-top
-" src="myimage2.jpg" alt="" />
+        <img
+          id="navimage"
+          className="w-full h-full object-cover object-left-top
+"
+          src="myimage2.jpg"
+          alt=""
+        />
+      </div>
+      <div id="nright" className={`absolute md:static  h-[30vh] md:h-0 ${open?"top-[-10px] w-full":"top-[-700px]"} left-[-8px] rounded-sm bg-zinc-600 flex items-center justify-center gap-3 m-2 md:m-0  flex-col md:flex md:flex-row  md:gap-4`}>
+        {["Home", "AboutMe", "ContactMe"].map((item, index) => (
+          <Link
+            onClick={handleLink}
+            key={index}
+            className="hover:opacity-[0.5]"
+            to={`/${index === 0 ? "" : item}`}
+          >
+            {item}
+          </Link>
+        ))}
+
 
       </div>
-      <div id="nright" className="flex gap-4">
-            {["Home","AboutMe","ContactMe"].map((item,index)=><Link key={index} className="hover:opacity-[0.5]" to={`/${index===0?"":item}`}>{item}</Link>)}
-      </div>
+      <span id="menu" className="absolute top-[37%] right-[2%]  md:hidden">
+            <i onClick={handleClick} className={`${open?"ri-close-fill":"ri-menu-line"}`}></i>
+      </span>
+      
     </div>
   );
 };
