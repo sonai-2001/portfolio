71eb158fdb070ebe72082bc4f1382a1322702c12
sonai-2001 <sonaibhadra11174@gmail.com>
2025-01-15 19:05:23 +0530

    marquee fixed

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { motion } from "framer-motion";
 
 function Marquee({ direction }) {
-  var names = ["JavaScript", "React", "Redux" ,"Tailwind", "React-Bootstrap"];
+  var names = ["JavaScript", "React", "Redux" ,"Tailwind"];
   return (
-    <div className="w-full flex  gap-4  overflow-hidden">
+    <div className="w-full flex gap-4  overflow-hidden">
       <motion.div
         initial={{ x: direction === "left" ? "0%" : "-100%" }}
         animate={{ x: direction === "left" ? "-100%" : "0%" }}
         transition={{ ease: "linear", duration: 15, repeat: Infinity }}
-        className="w-full  flex flex-shrink-0 items-center  "
+        className="w-full  flex flex-shrink-0 items-center   "
       >
         {names.map((elem) => (
           <div className=" mx-4 md:mx-auto md:w-[20vw]  flex items-center justify-center mb-10 flex-shrink-0 ">
@@ -21,7 +21,7 @@ function Marquee({ direction }) {
         initial={{ x: direction === "left" ? "0%" : "-100%" }}
         animate={{ x: direction === "left" ? "-100%" : "0%" }}
         transition={{ ease: "linear", duration: 15, repeat: Infinity }}
-        className="w-full  flex flex-shrink-0 items-center "
+        className="w-full  flex flex-shrink-0 items-center  "
       >
         {names.map((elem) => (
           <div className="mx-4 md:mx-auto md:w-[20vw] flex items-center justify-center mb-10 flex-shrink-0 ">
